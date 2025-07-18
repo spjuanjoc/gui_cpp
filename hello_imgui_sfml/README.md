356a7cd8f24669cbf7cc2ee64fa4c05dc5792179
spjuanjoc <juancho.312@hotmail.com>
2025-07-18 17:14:22 -0500

    build: upgrade to conan 2

--- hello_imgui_sfml/README.md
+++ hello_imgui_sfml/README.md
@@ -1,25 +1,15 @@
 # Hello ImGui
 
-This is a "Hello World" for ImGUI. ImGUI requires a backend.
-SFML works as the backend.
+This is a "Hello World" for ImGUI. ImGUI requires a backend. This example uses
+SFML as backend, with an already combined library ImGui-SFML
 
-This project contains the boilerplate code for imgui-SFML bindings
-for an easier integration between both.
+ImGui includes some features that go on top of SFML
 
-## Versions
+[Example](https://eliasdaler.github.io/using-imgui-with-sfml-pt1/)
 
-| Tool  | Version |
-|:------|:--------|
-| CMake | 3.20    |
-| std   | c++20   |
-| conan | 1.62    |
-| fmt   | 10.0.0  |
-| ImGUI | 1.90.5  |
-| SFML  | 2.6.1   |
+## Uses
 
-
-## See
-
-- [conan](https://conan.io/)
-- [ImGui](https://www.dearimgui.com/)
-- [SFML](https://www.sfml-dev.org/)
+- CMake > 3.24
+- std 23
+- conan 2.X
+- ImGUI + SFML
---
+++ hello_imgui_sfml/main.cpp
@@ -0,0 +1,47 @@
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <imgui-SFML.h>
+#include <imgui.h>
+
+void pollMainWindow(sf::RenderWindow& main_window, sf::Event& event)
+{
+  while (main_window.pollEvent(event))
+  {
+    ImGui::SFML::ProcessEvent(event);
+
+    if (event.type == sf::Event::Closed)
+    {
+      main_window.close();
+    }
+  }
+}
+
+int main()
+{
+  sf::RenderWindow main_window(sf::VideoMode(640, 480), "ImGui - Window");
+  main_window.setFramerateLimit(60);
+  sf::Clock delta_clock{};
+
+  [[maybe_unused]]
+  bool isLoaded = ImGui::SFML::Init(main_window);
+  ImGui::GetStyle().ScaleAllSizes(2.0f);
+  ImGui::GetIO().FontGlobalScale = 2.0f;
+
+  while (main_window.isOpen())
+  {
+    sf::Event event{};
+    pollMainWindow(main_window, event);
+    ImGui::SFML::Update(main_window, delta_clock.restart());
+
+    ImGui::Begin("Box1");
+    ImGui::TextUnformatted("Hello world from ImGUI + SFML");
+    ImGui::End();
+
+    main_window.clear();
+    ImGui::SFML::Render(main_window);
+    main_window.display();
+  }
+
+  ImGui::SFML::Shutdown();
+}
